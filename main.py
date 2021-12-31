31af5982ed831a4cace4fd6411f19909bb5a0de3
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-01-01 03:53:10 +0900

    all set.

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 from gensim.models import fasttext
-from gensim.test.utils import datapath
-
+import re
 import time
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-
 
 print("Loading...")
 start = time.time()
 model = fasttext.load_facebook_vectors("cc.ja.300.bin")
 end = time.time()
 print("Loaded.")
-print(end - start)
+print(f"time: {end-start}")
 print()
 
+while True:
+    positive_words = []
+    negative_words = []
+    input_text = input("単語を入力 > ")
+    if input_text == "end": break
 
-@app.route('/generate_words', methods=['POST'])
-def generate_words():
-    data = request.json
-    print(data)
+    negative_flg = False
+    str_tmp = ""
+    for c in input_text:
+        if c == '+' or c == '-':
+            if negative_flg:
+                negative_words.append(str_tmp)
+            else:
+                positive_words.append(str_tmp)
+            str_tmp = ""
+            negative_flg = (c == '-')
+            continue
+        str_tmp += c
+    if negative_flg:
+        negative_words.append(str_tmp)
+    else:
+        positive_words.append(str_tmp)
+    
+    positive_words = [word for word in set(positive_words) if len(word) > 0]
+    negative_words = [word for word in set(negative_words) if len(word) > 0]
 
-    positive_words = data['positive']
-    negative_words= data['negative']
-    topn = data['topn']
-
-    ans = model.most_similar(positive=positive_words, negative=negative_words, topn=topn)
-    ans = [{'word': x[0], 'similarity': x[1]} for x in ans]
-    return jsonify({'data': ans})
-
-
-app.run(debug=False, host='0.0.0.0')
+    start = time()
+    ans = [{'word': x[0], 'similarity': x[1]} for x in model.most_similar(positive=positive_words, negative=negative_words, topn=30)]
+    end = time()
+    print(ans)
+    print(f"time: {end-start}s")
